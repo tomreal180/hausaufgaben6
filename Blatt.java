0490523930b2c226a570e1977ca6ca4a4e947f34
MinhDucHa <ducha180102@gmail.com>
2022-11-15 09:05:49 +0100

    Exception werfen

--- Blatt.java
+++ Blatt.java
@@ -4,7 +4,7 @@ public class Blatt {
 	 private int[] res;
 	 public Blatt(int[] a) {
 		 if (a.length != 3) {
-			 throw new ArithmeticException("Es muss 3 karten haben");
+			throw new ArithmeticException("Es muss 3 karten geben");
 		 }else {
 			 this.res=a;
 		 }
